835a57128994f03de2260c20672da2d7bdf2258d
李昆翰 <kunhanl0511@gmail.com>
2025-12-19 14:42:07 +0800

    add page source

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://kunhanl.com',
+  output: 'static',
+  build: {
+    format: 'directory'
+  },
+  trailingSlash: 'never'
+});
+
