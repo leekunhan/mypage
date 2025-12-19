dfa6a0b7fd40af63a442757b12e2a10e801cace9
李昆翰 <kunhanl0511@gmail.com>
2025-12-19 14:53:41 +0800

    Fix base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kunhanl.com',
+  site: 'https://leekunhan.github.io/mypage/',
+  base: '/',
   output: 'static',
   build: {
     format: 'directory'
